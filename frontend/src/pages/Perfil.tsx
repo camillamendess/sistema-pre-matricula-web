60ea282782fa7e4efcb2f9166752c2ae36ec2cbf
Ademir Júnior <adejr0220@gmail.com>
2026-06-29 18:54:50 -0300

    feat: user profile data

--- frontend/src/pages/Perfil.tsx
+++ frontend/src/pages/Perfil.tsx
@@ -1,13 +1,37 @@
+import { useState, useEffect } from "react";
 import PagesLayout from "../layouts/PagesLayout";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Perfil() {
+  const { user } = useAuth();
+  useEffect(() => {
+   console.log("User data:", user);
+ }, [user]);
+
   return (
-      <PagesLayout
-          pageTitle="Perfil"
-          pageDescription="Visualize e gerencie as Perfil cadastradas no sistema."
-          userType="aluno"
-      >
-          <div>Lista de Perfil</div>
-      </PagesLayout>
-  )
+    <PagesLayout
+      pageTitle="Meu Perfil"
+      pageDescription="Visualize suas informações cadastradas no sistema."
+      userType="aluno"
+    >
+      <div className="bg-white p-8 rounded-xl shadow-sm border border-gray-200 w-full max-w-2xl">
+        <div className="space-y-6">
+        <div className="border-b border-gray-100 pb-4">
+            <h3 className="text-sm font-bold text-gray-400 uppercase tracking-wider">Nome Completo</h3>
+            <p className="text-lg text-[#322A6A] font-semibold">{user?.nome}</p>
+        </div>
+
+        <div className="border-b border-gray-100 pb-4">
+            <h3 className="text-sm font-bold text-gray-400 uppercase tracking-wider">E-mail Institucional</h3>
+            <p className="text-lg text-[#322A6A] font-semibold">{user?.email || "Não disponível"}</p>
+        </div>
+
+        <div className="pb-4">
+            <h3 className="text-sm font-bold text-gray-400 uppercase tracking-wider">Número de Matrícula</h3>
+            <p className="text-lg text-[#322A6A] font-semibold">{user?.matricula || "Não disponível"}</p>
+        </div>
+        </div>
+      </div>
+    </PagesLayout>
+  );
 }
